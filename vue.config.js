f019067f3049ddff5d76dea768a5de685aff499e
shenxingchao <864318466@qq.com>
2021-11-09 09:14:47 +0800

    1.升级依赖

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,18 @@ const fixNedbForElectronRenderer = {
 }
 
 module.exports = {
+  publicPath: '/vue-element-visual-layout/', //自己跟换部署的目录
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto'
+        }
+      ]
+    }
+  },
   chainWebpack: config => {
     // fix nedb
     config.resolve
